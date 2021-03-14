11d6b806f139adf68a663c62b664121c0f94a1b6
Victoria Iurina <Victoria_iurina@epam.com>
2021-03-14 20:16:53 +0300

    feat: Add solution for 'Brackets'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(typeof str !== 'string') {
+    return false;
+  }
+  let arr = str.split('');
+  let stack = [];
+  let openingBracket = [];
+  let closingBracket = [];
+  let openingBracketIndex;
+  let closingBracketIndex;
+
+  bracketsConfig.forEach(function (item) {
+    openingBracket.push(item[0]);
+    closingBracket.push(item[1]);
+  });
+
+  for(let i = 0; i < arr.length; i++) {
+    openingBracketIndex = openingBracket.indexOf(arr[i]);
+    closingBracketIndex = closingBracket.indexOf(arr[i]);
+    if(openingBracketIndex !== -1 && closingBracketIndex === -1) {
+      stack.push(openingBracketIndex);
+      continue;
+    }
+    if(openingBracketIndex !== -1 && closingBracketIndex !== -1 && stack.indexOf(openingBracketIndex) === -1) {
+      stack.push(openingBracketIndex);
+      continue;
+    }
+    if(closingBracketIndex !== -1) {
+      openingBracketIndex = stack.pop();
+      if(closingBracketIndex !== openingBracketIndex) {
+        return false;
+      }
+    }
+  }
+  return stack.length <= 0;
 }
